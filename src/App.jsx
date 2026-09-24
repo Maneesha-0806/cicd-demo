a030202a28ed77fc03363148cecd7ba6e56a98dc
Maneesha C A <maneeshaca08@gmail.com>
2026-09-24 10:30:50 +0530

    Updated welcome message

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div>
       <h1>Hello CI/CD</h1>
-      <p>This application is built automatically.</p>
+      <p>Thank you for using our application!</p>
     </div>
   );
 }
